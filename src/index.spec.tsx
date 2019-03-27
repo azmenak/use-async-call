52f5bf162d6516f8e8fadf53c969c7c87278db7b
Adam Zmenak <adam.zmenak@gmail.com>
2019-03-26 20:06:41 -0400

    100% coverage!

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -44,6 +44,30 @@ describe('use-async-call', () => {
     expect(onSuccess).toHaveBeenCalledWith(true)
   })
 
+  it('calls onComplete callback in success and failure cases', async () => {
+    const call = jest.fn(() => Promise.resolve(true))
+    const onComplete = jest.fn()
+
+    const {waitForNextUpdate, rerender} = renderHook(
+      ({caller}) => useAsyncCall(caller, {onComplete}),
+      {initialProps: {caller: call}}
+    )
+
+    expect(onComplete).not.toHaveBeenCalled()
+    await waitForNextUpdate()
+
+    expect(onComplete).toHaveBeenCalledTimes(1)
+
+    const failCall = jest.fn(() => Promise.reject(new Error()))
+
+    rerender({caller: failCall})
+
+    expect(onComplete).toHaveBeenCalledTimes(1)
+    await waitForNextUpdate()
+
+    expect(onComplete).toHaveBeenCalledTimes(2)
+  })
+
   it('sets the error in response after rejection', async () => {
     const err = new Error()
     const call = jest.fn(() => Promise.reject(err))
@@ -80,8 +104,9 @@ describe('use-async-call', () => {
   it('does not call handlers when the input method changes', async () => {
     const call = jest.fn(() => Promise.resolve(true))
     const onSuccess = jest.fn()
+    const onFailure = jest.fn()
     const {waitForNextUpdate, rerender} = renderHook(
-      ({caller}) => useAsyncCall(caller, {onSuccess}),
+      ({caller}) => useAsyncCall(caller, {onSuccess, onFailure}),
       {initialProps: {caller: call}}
     )
 
@@ -96,6 +121,18 @@ describe('use-async-call', () => {
 
     expect(onSuccess).toHaveBeenCalledTimes(1)
     expect(onSuccess).toHaveBeenCalledWith(false)
+
+    const callFail = jest.fn(() => Promise.reject(new Error()))
+    rerender({caller: callFail})
+
+    expect(onFailure).not.toHaveBeenCalled()
+
+    const nextCallFail = jest.fn(() => Promise.reject(new Error()))
+    rerender({caller: nextCallFail})
+
+    await waitForNextUpdate()
+
+    expect(onFailure).toHaveBeenCalledTimes(1)
   })
 
   it('calls to update function, update the loading state and data state', async () => {
@@ -125,5 +162,130 @@ describe('use-async-call', () => {
     expect(result.current[0].loading).toBe(false)
     expect(result.current[0].data).toBe(false)
     expect(onSuccess).toHaveBeenCalledWith(false)
+  })
+
+  it('does not update state for update failures', async () => {
+    const call = jest.fn(() => Promise.resolve(true))
+    const {result, waitForNextUpdate} = renderHook(() => useAsyncCall(call))
+
+    await waitForNextUpdate()
+
+    const error = new Error()
+    const updateFail = jest.fn(() => Promise.reject(error))
+    const onFailure = jest.fn()
+    const onComplete = jest.fn()
+
+    act(() => {
+      result.current[1](updateFail, {onFailure, onComplete})
+    })
+
+    await waitForNextUpdate()
+
+    expect(result.current[0].loading).toBe(false)
+    expect(result.current[0].data).toBe(true)
+    expect(result.current[0].error).toBe(null)
+    expect(onFailure).toHaveBeenCalledWith(error)
+    expect(onComplete).toHaveBeenCalledTimes(1)
+  })
+
+  it('updates state for update failures', async () => {
+    const call = jest.fn(() => Promise.resolve(true))
+    const {result, waitForNextUpdate} = renderHook(() => useAsyncCall(call))
+
+    await waitForNextUpdate()
+
+    const update = jest.fn(() => Promise.resolve(false))
+    act(() => {
+      result.current[1](update)
+    })
+
+    await waitForNextUpdate()
+
+    expect(result.current[0].loading).toBe(false)
+    expect(result.current[0].data).toBe(false)
+    expect(result.current[0].error).toBe(null)
+  })
+
+  it('implements default update behaviour when no options passed', async () => {
+    const call = jest.fn(() => Promise.resolve(true))
+    const {result, waitForNextUpdate} = renderHook(() => useAsyncCall(call))
+
+    await waitForNextUpdate()
+
+    const error = new Error()
+    const updateFail = jest.fn(() => Promise.reject(error))
+
+    act(() => {
+      result.current[1](updateFail, {saveError: true})
+    })
+
+    await waitForNextUpdate()
+
+    expect(result.current[0].loading).toBe(false)
+    expect(result.current[0].data).toBe(null)
+    expect(result.current[0].error).toBe(error)
+  })
+
+  it('throws an error when rejecting with throw config', async () => {
+    expect.assertions(1)
+
+    const call = jest.fn(() => Promise.resolve(true))
+    const {result, waitForNextUpdate} = renderHook(() => useAsyncCall(call))
+
+    await waitForNextUpdate()
+
+    const error = new Error()
+    const updateFail = jest.fn(() => Promise.reject(error))
+
+    try {
+      await result.current[1](updateFail, {throwError: true})
+    } catch (err) {
+      expect(err).toBe(error)
+    }
+  })
+
+  it('update works with promise instead of method argument', async () => {
+    const call = jest.fn(() => Promise.resolve(true))
+    const {result, waitForNextUpdate} = renderHook(() => useAsyncCall(call))
+
+    await waitForNextUpdate()
+
+    const onSuccess = jest.fn()
+
+    act(() => {
+      result.current[1](Promise.resolve(false), {onSuccess})
+    })
+
+    expect(onSuccess).not.toBeCalled()
+
+    await waitForNextUpdate()
+
+    expect(onSuccess).toHaveBeenCalledWith(false)
+  })
+
+  it('calls the async caller when returned refresh method is called', async () => {
+    let count = 0
+    const call = jest.fn(() => Promise.resolve(count++))
+    const {result, waitForNextUpdate} = renderHook(() => useAsyncCall(call))
+
+    expect(result.current[0].data).toBe(null)
+    expect(call).toHaveBeenCalledTimes(1)
+
+    await waitForNextUpdate()
+
+    expect(result.current[0].data).toBe(0)
+
+    act(() => {
+      result.current[2]()
+    })
+
+    expect(call).toHaveBeenCalledTimes(2)
+    expect(result.current[0].data).toBe(0)
+    expect(result.current[0].loading).toBe(true)
+
+    await waitForNextUpdate()
+
+    expect(result.current[0].data).toBe(1)
+    expect(result.current[0].loading).toBe(false)
   })
 })
